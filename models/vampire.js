6a05b840e02240bbf21eb95d648826feb81bd133
Hisham Aljahbli <h.aljahbli@gmail.com>
2019-12-02 17:00:50 +0300

    schema built

--- models/vampire.js
+++ models/vampire.js
@@ -3,6 +3,15 @@ const Schema = mongoose.Schema;
 
 const vampireSchema = new Schema({
   // add your code here to set up your schema
+  name: {type:String, required:true},
+  title: String,
+  hair_color:{type:String, default:'blonde'},
+  eye_color: String,
+  dob: Date,
+  loves: [String],
+  location: String,
+  gender: String,
+  victims: {type: Number, default: 1}
 });
 
 const Vampire = mongoose.model('Vampire', vampireSchema);
