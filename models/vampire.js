f69ff372efd232180b1a7093082ed80b1562efca
Lameesaf <lafallaj2@gmail.com>
2019-12-02 21:47:55 +0300

    add schema

--- models/vampire.js
+++ models/vampire.js
@@ -3,7 +3,19 @@ const Schema = mongoose.Schema;
 
 const vampireSchema = new Schema({
   // add your code here to set up your schema
-});
+  name: {type: String, required: true},
+  hair_color: {type: String, default: 'blonde'},
+  eye_color: String,
+  dob: Date,
+  loves: [String],
+  location: String,
+  gender: String,
+  title: String,
+  victims: {type: Number, default: 0, $gte: 0},
+  is_actually: String,
+  portrayed_by: String,
+  hates: [String],
+},{timestamps: true});
 
 const Vampire = mongoose.model('Vampire', vampireSchema);
 
